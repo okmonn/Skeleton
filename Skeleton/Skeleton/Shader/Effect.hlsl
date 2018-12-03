647bc66f0a58981735a10ccd1a53d27a3d379bcd
okmonn <35838332+okmonn@users.noreply.github.com>
2018-12-03 14:09:32 +0900

    add

--- Skeleton/Skeleton/Shader/Effect.hlsl
+++ Skeleton/Skeleton/Shader/Effect.hlsl
@@ -129,8 +129,8 @@ void Limiter(uint index)
     }
 }
 
-// フェード
-void Fade(uint index)
+// フェードイン
+void FadeIn(uint index)
 {
     real[index] = origin[index];
     uint2 size;
@@ -141,6 +141,20 @@ void Fade(uint index)
     if(index < size.x * duration)
     {
         real[index] *= index / (size.x * duration);
+    }
+}
+
+// フェードアウト
+void FadeOut(uint index)
+{
+    real[index] = origin[index];
+    uint2 size;
+    origin.GetDimensions(size.x, size.y);
+
+    //適応時間
+    float duration = 0.5f;
+    if (index < size.x * duration)
+    {
         real[size.x - index - 1] *= index / (size.x * duration);
     }
 }
@@ -153,20 +167,84 @@ groupshared float output[2];
 // IIR ローパス
 void LowPass(uint index)
 {
-    float cutoff = 20000.0f;
+    float cutoff = 1000.0f;
     float q = 1.0f / sqrt(2.0f);
 
-    //フィルタ係数計算で使用する中間値を求める。
+    //フィルタ係数計算で使用する中間値
     float omega = 2.0f * PI * cutoff / sample;
     float alpha = sin(omega) / (2.0f * q);
 
-	//フィルタ係数を求める。
+	//フィルタ係数
     float a0 =  1.0f + alpha;
     float a1 = -2.0f * cos(omega);
     float a2 =  1.0f - alpha;
     float b0 = (1.0f - cos(omega)) / 2.0f;
     float b1 =  1.0f - cos(omega);
     float b2 = (1.0f - cos(omega)) / 2.0f;
+
+    real[index] = (b0 / a0 * origin[index]) + (b1 / a0 * input[0]) + (b2 / a0 * input[1]) - (a1 / a0 * output[0]) - (a2 / a0 * output[1]);
+
+    //入力バッファ
+    input[1] = input[0];
+    input[0] = origin[index];
+
+    //出力バッファ
+    output[1] = output[0];
+    output[0] = real[index];
+
+    //計算が終わるまでほかのスレッドは待機
+    GroupMemoryBarrierWithGroupSync();
+}
+
+// IIR ハイパス
+void HightPass(uint index)
+{
+    float cutoff = 1000.0f;
+    float q = 1.0f / sqrt(2.0f);
+
+    //フィルタ係数計算で使用する中間値
+    float omega = 2.0f * PI * cutoff / sample;
+    float alpha = sin(omega) / (2.0f * q);
+
+	//フィルタ係数
+    float a0 =  1.0f + alpha;
+    float a1 = -2.0f * cos(omega);
+    float a2 =  1.0f - alpha;
+    float b0 = (1.0f + cos(omega)) / 2.0f;
+    float b1 =  1.0f + cos(omega);
+    float b2 = (1.0f + cos(omega)) / 2.0f;
+
+    real[index] = (b0 / a0 * origin[index]) + (b1 / a0 * input[0]) + (b2 / a0 * input[1]) - (a1 / a0 * output[0]) - (a2 / a0 * output[1]);
+
+    //入力バッファ
+    input[1] = input[0];
+    input[0] = origin[index];
+
+    //出力バッファ
+    output[1] = output[0];
+    output[0] = real[index];
+
+    //計算が終わるまでほかのスレッドは待機
+    GroupMemoryBarrierWithGroupSync();
+}
+
+// IIR バンドパス
+void BundPass(uint index)
+{
+    float cutoff = 1000.0f;
+    float q = 1.0f / sqrt(2.0f);
+
+    //フィルタ係数計算で使用する中間値
+    float omega = 2.0f * PI * cutoff / sample;
+    float alpha = sin(omega) / (2.0f * q);
+
+	//フィルタ係数
+    float a0 =  1.0f + alpha;
+    float a1 = -2.0f * cos(omega);
+    float a2 =  1.0f - alpha;
+    float b0 =  alpha;
+    float b1 =  0.0f;
+    float b2 = -alpha;
 
     real[index] = (b0 / a0 * origin[index]) + (b1 / a0 * input[0]) + (b2 / a0 * input[1]) - (a1 / a0 * output[0]) - (a2 / a0 * output[1]);
 
