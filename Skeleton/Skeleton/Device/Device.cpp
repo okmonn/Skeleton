148d79aac02a421bff42d855ba89d3bfdd6c936f
okmonn <35838332+okmonn@users.noreply.github.com>
2019-03-11 09:57:27 +0900

    add

--- Skeleton/Skeleton/Device/Device.cpp
+++ Skeleton/Skeleton/Device/Device.cpp
@@ -1,39 +1,105 @@
+#include "Device.h"
 #include "../Release.h"
+#include <vector>
 
 // 機能レベル一覧
 const D3D_FEATURE_LEVEL levels[] = {
 	D3D_FEATURE_LEVEL::D3D_FEATURE_LEVEL_12_1,
 	D3D_FEATURE_LEVEL::D3D_FEATURE_LEVEL_12_0,
 	D3D_FEATURE_LEVEL::D3D_FEATURE_LEVEL_11_1,
-	D3D_FEATURE_LEVEL::D3D_FEATURE_LEVEL_11_0
+	D3D_FEATURE_LEVEL::D3D_FEATURE_LEVEL_11_0,
 };
 
 // コンストラクタ
-Device::Device() : 
-	dev(nullptr)
+Device::Device() :
+	dev(nullptr), factory(nullptr)
 {
-	CreateDev();
+	Init();
 }
 
 // デストラクタ
 Device::~Device()
 {
+	Release(factory);
 	Release(dev);
 }
 
-// デバイスの生成
+// ファクトリー生成
+long Device::CreateFactory(void)
+{
+	auto hr = CreateDXGIFactory(IID_PPV_ARGS(&factory));
+	if (FAILED(hr))
+	{
+		OutputDebugString(_T("\nファクトリー生成：失敗\n"));
+	}
+
+	return hr;
+}
+
+// デバイス生成
 long Device::CreateDev(void)
 {
-	long hr = S_OK;
+	//アダプターの検索
+	std::vector<IDXGIAdapter*>adapters;
+	IDXGIAdapter* adapter = nullptr;
+	for (UINT i = 0; factory->EnumAdapters(i, &adapter) != DXGI_ERROR_NOT_FOUND; ++i)
+	{
+		adapters.push_back(adapter);
+	}
+	for (auto& i : adapters)
+	{
+		DXGI_ADAPTER_DESC desc{};
+		i->GetDesc(&desc);
+		std::wstring name = desc.Description;
+		if (name.find(L"NVIDIA") != std::string::npos)
+		{
+			adapter = i;
+			break;
+		}
+		if (name.find(L"AMD") != std::string::npos)
+		{
+			adapter = adapters[0];
+			break;
+		}
+	}
+
+	auto hr = S_OK;
 	for (auto& i : levels)
 	{
-		hr = D3D12CreateDevice(nullptr, i, IID_PPV_ARGS(&dev));
+		hr = D3D12CreateDevice(adapter, i, IID_PPV_ARGS(&dev));
 		if (hr == S_OK)
 		{
-			OutputDebugString(_T("\nデバイスの生成：成功\n"));
+			OutputDebugString(_T("\nデバイス生成：成功\n"));
+			Release(adapter);
 			break;
 		}
 	}
 
 	return hr;
+}
+
+// 初期化
+void Device::Init(void)
+{
+	CreateFactory();
+	CreateDev();
+}
+
+// インスタンス変数取得
+Device & Device::Get(void)
+{
+	static Device instance;
+	return instance;
+}
+
+// デバイス取得
+ID3D12Device * Device::GetDev(void) const
+{
+	return dev;
+}
+
+// ファクトリー取得
+IDXGIFactory4 * Device::GetFactory(void) const
+{
+	return factory;
 }
--- Skeleton/Skeleton/Device/Device.h
+++ Skeleton/Skeleton/Device/Device.h
@@ -1,34 +1,41 @@
 #pragma once
 
 struct ID3D12Device;
+struct IDXGIFactory4;
 
+// デバイス
 class Device
 {
 public:
 	// デストラクタ
 	~Device();
 
-	// インスタンス変数の取得
-	static Device& Get(void) {
-		static Device instance;
-		return instance;
-	}
+	// インスタンス変数取得
+	static Device& Get(void);
 
-	// デバイスの取得
-	ID3D12Device* GetDev(void) const {
-		return dev;
-	}
+	// デバイス取得
+	ID3D12Device* GetDev(void) const;
+	// ファクトリー取得
+	IDXGIFactory4* GetFactory(void) const;
 
 private:
-	// コンストラクタ
 	Device();
 	Device(const Device&) = delete;
 	void operator=(const Device&) = delete;
 
-	// デバイスの生成
+	// ファクトリー生成
+	long CreateFactory(void);
+
+	// デバイス生成
 	long CreateDev(void);
+
+	// 初期化
+	void Init(void);
 
 
 	// デバイス
 	ID3D12Device* dev;
+
+	// ファクトリー
+	IDXGIFactory4* factory;
 };
